b20a0f6c56704562b0bf60b02d98ea57746b3434
ninogueira <ninogueira@uolinc.com>
2021-05-22 16:57:55 -0300

    Commit inicial

---
+++ src/test/java/br/com/meeting/adm/AdmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meeting.adm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
