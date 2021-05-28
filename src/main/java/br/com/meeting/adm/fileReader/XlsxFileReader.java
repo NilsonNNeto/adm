fc63a50365225a9819be385b78c63985e85b76df
ninogueira <ninogueira@uolinc.com>
2021-05-28 15:57:24 -0300

    Altera leitura de arquivo para não criar cópia do mesmo na raiz do projeto

--- src/main/java/br/com/meeting/adm/fileReader/XlsxFileReader.java
+++ src/main/java/br/com/meeting/adm/fileReader/XlsxFileReader.java
@@ -3,27 +3,19 @@ package br.com.meeting.adm.fileReader;
 import br.com.meeting.adm.entity.ParticipationFormEntity;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 public class XlsxFileReader {
 
-    private final String PATH = "/home/ninogueira/Documents/Nilson/Desafios";
-    private final String NAME = "Fichas Mrnta.xlsx";
+    public List<ParticipationFormEntity> readFileBuildForms(MultipartFile multipartFile) throws IOException {
 
-    public List<ParticipationFormEntity> readFileBuildForms() throws IOException {
-        File file = new File(PATH + "/" + NAME);
-
-        if (file == null) {
-            return new LinkedList<>();
-        }
-
-        Sheet sheet = new XSSFWorkbook(new FileInputStream(file)).getSheetAt(0);
+        Sheet sheet = getFirstSheetFromMultipartFile(multipartFile);
 
         List<ParticipationFormEntity> forms = new LinkedList<>();
 
@@ -32,15 +24,20 @@ public class XlsxFileReader {
             if (row.getRowNum() == sheet.getFirstRowNum() || row.getCell(0) == null) return;
 
             forms.add(ParticipationFormEntity.builder()
-                    .id(new Double(cellValue(row, 1)).intValue())
+                    .id(Double.valueOf((cellValue(row, 1))).intValue())
                     .gender(cellValue(row, 5))
-                    .height(new Double(cellValue(row, 4)))
+                    .height(Double.valueOf(cellValue(row, 4)))
                     .church(cellValue(row, 15).isEmpty() ? cellValue(row, 14) : cellValue(row, 15))
                     .location(cellValue(row, 10))
                     .build());
         });
 
         return forms;
+    }
+
+    private Sheet getFirstSheetFromMultipartFile(MultipartFile multipartFile) throws IOException {
+        ByteArrayInputStream stream = new ByteArrayInputStream(multipartFile.getBytes());
+        return WorkbookFactory.create(stream).getSheetAt(0);
     }
 
     private String cellValue(Row row, int cellNum) {
